eb431e6797754039ac906ec11b22346620bfa782
Mariana Victorino <victorino.marii@gmail.com>
2024-10-03 15:28:43 +0100

    lab js functions and arrays done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,46 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    else if (num1 < num2) {
+        return num2;
+    }
+    else {
+        return num1, num2;
+    }
+};
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) {
+    return null;
+    }
+let longestWord = words [0]; //se a primeira for maior
 
+for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+        longestWord = words[i]; //atualiza a variavel quando a palavra é maior
+    }
+}
+return longestWord;
 
-
+};
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0; 
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum +=numbers[i]; // adiciona o numero da vez à soma
+    }
+    return sum;
+};
 
 
 
@@ -23,7 +48,14 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+
+    if (numbers.length === 0) {
+        return 0;
+    }
+    let sum = sumNumbers(numbers);
+    return sum / numbers.length;
+};
 
 
 
@@ -31,4 +63,19 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordArr, searchedWord) {
+    if (wordArr.length === 0) {
+        return null;
+    }
+
+    for (let i = 0; i < wordArr.length; i++) {
+        if (wordArr[i] === searchedWord) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+
